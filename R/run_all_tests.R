13ae0bbf152f09950c82982128d19d673b1bddfd
jwschroeder3 <schroedj@umich.edu>
2024-01-26 16:31:51 -0500

    reduced model number

--- R/run_all_tests.R
+++
@@ -1,8 +0,0 @@
-# Load the required package
-library(testthat)
-
-# Use the function test_dir to run all the tests inside 'tests' directory
-result <- test_dir("tests/") 
-
-# view the result
-print(result)
